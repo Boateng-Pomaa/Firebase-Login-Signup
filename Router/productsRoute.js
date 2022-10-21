2fab2307b0e43b660e94539367341ad8149391c2
Boateng-Pomaa <lindapomaa27@gmail.com>
2022-10-21 12:56:09 +0100

    update

--- Router/productsRoute.js
+++ Router/productsRoute.js
@@ -3,12 +3,14 @@ const router = express.Router()
 
 import registerProducts from '../Controller/products.js'
 import getProducts from '../Controller/products.js'
-
+import uploadFile from '../Controller/products.js'
 
 router.
       post('/products', registerProducts)
 router
       .get('/allproducts', getProducts)
+router.
+      post('/upload', uploadFile)
 
 
 
